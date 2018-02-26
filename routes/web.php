c9aa3906b1d470461325b02df9700ff07994375c
samuel <samderrty123@gmail.com>
2018-02-26 10:46:49 -0500

    blahhl/

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,47 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
+});
+Route::get('/about', function () {
+    return view('pages.about');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
+});
+Route::post('/contact', function () {
+
+    $data = request()->all();
+
+    echo "Email:" . $data['email'] . '<br>';
+    echo "Subject:" . $data ['subject'] . '<br>';
+    echo "Body:" . $data['body'] . '<br>';
+
+});
+Route::get('/login', function () {
+    return view('pages.login');
+});
+Route::post('/login', function () {
+
+    $data = request()->all();
+
+    echo "Email:" . $data['email'] . '<br>';
+    echo "Password:" . $data['password'];
+});
+
+Route::get('/signup', function () {
+    return view('pages.signup');
+});
+Route::post('/signup', function () {
+
+    $data = request()->all();
+
+    echo "Email:" . $data['email'] . '<br>';
+    echo "Password:" . $data['password'];
+});
+Route::get('/view messages', function () {
+    return view('pages.view messages');
+});
+Route::get('/logout', function () {
+    return view('pages.logout');
 });
