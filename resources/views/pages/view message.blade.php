b15e2bd93cf4573dc99cc79352234f137f2739f0
samuel <samderrty123@gmail.com>
2018-02-26 18:15:16 -0500

    created A bootcard included a colon. edited text edited more edited super editing

--- resources/views/pages/view message.blade.php
+++ resources/views/pages/view message.blade.php
@@ -8,33 +8,33 @@
 
 
 
-    <table class="table">
-        <thead>
-        <tr>
+        <table class="table">
+            <thead>
+            <tr>
 
-            <th scope="col"></th>
-            <th scope="col">All Messages</th>
-            <th scope="col">Sender's Name</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td><a href="/bootcard">Message</a></td>
-            <td>Messi</td>
-            <td><button type="submit" class="btn btn-primary mb-2">Delete</button>  </td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td><a href="bootcard">Message</a></td>
-            <td>Ronaldo</td>
-            <td><button type="submit" class="btn btn-primary mb-2">Delete</button>     </td>
-        </tr>
-        <tr>
+                <th scope="col"></th>
+                <th scope="col">All Messages</th>
+                <th scope="col">Sender's Name</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td><a href="/bootcard">Message</a></td>
+                <td>Messi</td>
+                <td><button type="submit" class="btn btn-primary mb-2">Delete</button>  </td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td><a href="bootcard">Message</a></td>
+                <td>Ronaldo</td>
+                <td><button type="submit" class="btn btn-primary mb-2">Delete</button>     </td>
+            </tr>
+            <tr>
 
-        </tr>
-        </tbody>
-    </table>
+            </tr>
+            </tbody>
+        </table>
 
 
 
